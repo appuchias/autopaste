bf39e055a8680ba21dece7f188befda5208d2daf
Appúchia <46938448+appuchias@users.noreply.github.com>
2020-06-26 14:11:52 +0200

    Accept any file path

--- autopaste.py
+++ autopaste.py
@@ -2,7 +2,15 @@ import keyboard, pyperclip, os
 
 fn = input("Type the filename:\n> ")
 
-path = os.path.join("D:\Pablo\Documentos\PastedTxts", fn)
+try:
+    with open("fp.txt") as r:
+        fp = r.read()
+except FileNotFoundError:
+    with open("fp.txt", "x"):
+        pass
+    fp = input("Type the folder where to save all the files:\n> ")
+
+path = os.path.join(fp, fn)
 
 with open(path, "a") as a:
     while True:
